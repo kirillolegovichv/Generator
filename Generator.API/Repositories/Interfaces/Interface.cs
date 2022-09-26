cd5e9bb993d23009d56c4490b812d581cfd8d275
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-26 12:40:58 +0300

    Create interface

---
+++ Generator.API/Repositories/Interfaces/Interface.cs
@@ -0,0 +1,9 @@
+﻿using Generator.API.Models;
+
+namespace Generator.API.Repositories
+{
+    public interface IBaseRepository<TDbModel> where TDbModel : BaseModel
+    {
+        public TDbModel Create(TDbModel model);
+    }
+}
