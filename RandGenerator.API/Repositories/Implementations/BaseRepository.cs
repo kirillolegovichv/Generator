0addf1c1c6f781c56908b8f6cb94615369de1eb6
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-28 13:02:12 +0300

    Add BaseRepository

---
+++ RandGenerator.API/Repositories/Implementations/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using RandGenerator.API.Models;
+
+namespace RandGenerator.API.Repositories
+{
+    public class BaseRepository<Model> : IBaseRepository<Model> where Model : GeneratedSequence
+    {
+        public Model Create(Model model)
+        {
+            return model;
+        }
+    }
+}
