00245ac53ff10b831d4dcfae18ec83170b96f5a2
Kirill <kirill.vasil.oleg@gmail.com>
2022-09-28 12:56:44 +0300

    Add interface

---
+++ RandGenerator.API/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using RandGenerator.API.Models;
+
+namespace RandGenerator.API.Repositories
+{
+    public interface IBaseRepository<Model> where Model : GeneratedSequence
+    {
+        public Model Create(Model model);
+    }
+}
