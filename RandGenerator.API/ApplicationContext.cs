188ee2a4a07d6ad032b0b05eb8b78303919e63ce
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-02 19:29:46 +0300

    Add class AplicationComtext, change method BaseRepository

---
+++ RandGenerator.API/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RandGenerator.API.Models;
+
+namespace RandGenerator.API
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<GeneratedSequence> Sequences { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+        {
+
+        }
+    }
+}
